22bb7c26b620443f4e409acf24d0a3219e11fc71
Gianmarco Polizzi <65085829+GianPolizzi@users.noreply.github.com>
2022-01-11 16:00:40 +0100

    Delete class child inside parent

--- lez_3/ereditarietà/veicolo.js
+++ lez_3/ereditarietà/veicolo.js
@@ -13,24 +13,3 @@ class Veicolo {
 
 const veicolo1 = new Veicolo(4, false);
 console.log(veicolo1.descrizioneVeicolo());
-
-
-//CLASSE AUTO (FIGLIO)
-
-class Auto extends Veicolo {
-    constructor(ruote, motore, marca, colore){
-        //Con super richiamo i parametri della classe padre
-        super(ruote, motore);
-        //Con this inizializzo i nuovi parametri
-        this.marca = marca;
-        this.colore = colore;
-    }
-
-    descrizioneAuto(){
-        return `${super.descrizioneVeicolo()} ed in particolare è un auto ${this.marca} di colore 
-        ${this.colore}`;
-    }
-}
-
-const auto1 = new Auto(4, true, 'Audi', 'bianco');
-console.log(auto1.descrizioneAuto());
